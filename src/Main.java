90e5df4585f251cb7c8d98a7a1637a49a3e43211
mukhammad aziz <inobiddinovmuhammadaziz@gmail.com>
2023-03-05 12:30:42 +0800

    Creating more function to make it code more readable

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args){
-    Assignment1B_Mukhammadaziz archery = new Assignment1B_Mukhammadaziz();
-    archery.order();
+    public static void main(String[] args) {
+        Assignment1B_Mukhammadaziz archery = new Assignment1B_Mukhammadaziz();
+        archery.order();
     }
 }
